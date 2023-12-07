323a0b10f031d627bd4fb96a885d3498f75dc63d
Helina0610 <hongjuy0610@gmail.com>
2023-12-07 16:49:48 +0900

    feat : 설정파일 수정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [
+      "images.unsplash.com",
+      "s3.us-west-2.amazonaws.com",
+      "www.notion.so",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
