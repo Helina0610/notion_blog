16b8df105dfde74358ab0e84ce73997d12e6201a
Helina0610 <hongjuy0610@gmail.com>
2023-12-08 16:30:06 +0900

    feat : 말풍선 스타일 변경

--- components/intro/HeroSection.tsx
+++ components/intro/HeroSection.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 export const HeroSection = () => {
   return (
 		<section>
-			<div className='flex flex-col md:items-center gap-8 py-16 w-11/12 mx-auto text-center md:text-left max-w-7xl'>
+			<div className='flex flex-col md:items-center gap-8 py-24 w-11/12 mx-auto text-center md:text-left max-w-7xl'>
 				<div className='relative' >
 					<span className='absolute left-1/2 -bottom-2 -translate-x-1/2 rotate-45 bg-black w-6 h-6'></span>
 					<span className='font-bold text-white bg-black py-2 px-2 rounded-lg relative'>Hello</span>
