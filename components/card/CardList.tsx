d21934cf4141af741e3280c3662e0a8e0403bbb3
Helina0610 <97938033+Helina0610@users.noreply.github.com>
2023-12-28 14:44:32 +0900

    feat : 수정의 수정

--- components/card/CardList.tsx
+++ components/card/CardList.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import CardItem from './CardItem';
+import { ParsedDatabaseItemType } from '@/utils/parseDatabaseItems';
 
-const CardList = () => {
+interface CardListProps {
+	cardItems : ParsedDatabaseItemType[]
+}
+
+const CardList = ({cardItems} : CardListProps) => {
   return (
     <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8'>
-      <CardItem />
+      {
+				cardItems.map((cardItem) => (
+					<CardItem key={cardItem.id} cardItem={cardItem} />
+				))
+			}
     </ul>
   )
 }
--- components/intro/CardSection.tsx
+++ components/intro/CardSection.tsx
@@ -1,16 +1,18 @@
+import { ParsedDatabaseItemType } from '@/utils/parseDatabaseItems';
 import React from 'react'
+import CardList from '../card/CardList';
 
 interface CardSectionProps {
   cardItems: ParsedDatabaseItemType[];
 }
 
 export const CardSection = ({cardItems} : CardSectionProps) => {
-export const CardSection = () => {
   return (
     <section>
       <div className="max-w-5xl w-4/5 mx-auto flex flex-col gap-6 py-8">
         <hr/>
         <h3 className="font-bold text-3xl">Posts</h3>
+        <CardList cardItems={cardItems}/>
       </div>
     </section>
   )
