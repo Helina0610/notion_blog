bcbde2814969a6953c5eaec502afb2b4652824d7
Helina0610 <97938033+Helina0610@users.noreply.github.com>
2023-12-28 13:50:30 +0900

    Revert "feat: 데이터 연결"

--- components/card/CardItem.tsx
+++ components/card/CardItem.tsx
@@ -1,31 +1,17 @@
-import { ParsedDatabaseItemType } from '@/utils/parseDatabaseItems'
-import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-interface CardItemProps {
-	cardItem : ParsedDatabaseItemType
-}
-
-const CardItem = ({cardItem} : CardItemProps) => {
-  console.log("푸바오");
-  console.log(cardItem.cover);
-  const {description,icon,id,published, tags,title, previewImage, cover} = {...cardItem}
+const CardItem = () => {
   return (
     <li className='rounded-3xl overflow-hidden shadow-lg group flex  flex-col'>
-        <Link href=''>
-          <div className='relative aspect-[1.3/1]'>
-            {/* <Image src={cover} alt={title} layout='fill' className=' group-hover:scale-105 translate-transform' /> */}
-          </div>
-          <div className='p-6 flext flex-col gap-4 '>
-            <h4 className='font-bold text-2xl group-hover:text-blue-600 transition-colors flex flex-row items-center gap-1'>
-              {/* <IconRenderer icon={icon} alt={title} proxyIconUrl={proxy.icon}/> */}
-              {title}
-            </h4>
-            { description ? (<p className='font-medium text-gray-600'>{description}</p>) : null}
-            <time className='font-medium text-gray-700'>{published}</time>
-          </div>
-				</Link>
+      <Link href={''}>
+        <div className='relative aspect-[1.3/1]'>
+          
+        </div>
+        <div className='p-6 flext flex-col gap-4 '>
+          <h4 className='font-bold text-2xl group-hover:text-blue-600 transition-colors flex flex-row items-center gap-1'>title</h4>       
+        </div>
+      </Link>
     </li>
   )
 }
--- components/intro/CardSection.tsx
+++ components/intro/CardSection.tsx
@@ -1,20 +1,11 @@
 import React from 'react'
-import CardList from '../card/CardList';
-import { ParsedDatabaseItemType } from '@/utils/parseDatabaseItems';
 
-interface CardSectionProps {
-  cardItems: ParsedDatabaseItemType[];
-}
-
-export const CardSection = ({cardItems} : CardSectionProps) => {
-  console.log("푸바오 귀여웡")
-  console.log(cardItems);
+export const CardSection = () => {
   return (
     <section>
       <div className="max-w-5xl w-4/5 mx-auto flex flex-col gap-6 py-8">
         <hr/>
         <h3 className="font-bold text-3xl">Posts</h3>
-        <CardList cardItems={cardItems}/>
       </div>
     </section>
   )
