ba70ff97dbd000388fbfa9bf37167d061d8ea256
Helina0610 <97938033+Helina0610@users.noreply.github.com>
2024-01-01 15:03:32 +0900

    feat : Notion Starter Kit

--- app/posts/[pageId]/page.tsx
+++ app/posts/[pageId]/page.tsx
@@ -1,6 +1,8 @@
 
+import { PageHead } from '@/components/layout/PageHead';
 import NotionPageRenderer from '@/components/notion/NotionPageRenderer';
 import { NotionAPI } from 'notion-client';
+import { getPageProperty, getPageTitle } from 'notion-utils';
 import React, { cache } from 'react'
 
 interface PostParams{
@@ -10,6 +12,12 @@ interface PostParams{
 export default async function Posts  ({params} : PostParams)  {
   const notion = new NotionAPI();
   const recordMap = await notion.getPage(params.pageId);
+
+  // const propertyValue = Object.values(recordMap.block)[0].value;
+  // const title = getPageTitle(recordMap);
+  // const keywords = getPageProperty<string[]>("태그", propertyValue, recordMap).join(",");
+  // const description = getPageProperty<string>("설명", propertyValue, recordMap);
+
   return (
     <>
       {/* <PageHead title={title} description={description} keywords={keywords} image={ogImage}/> */}
--- components/layout/PageHeader.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const PageHeader = () => {
-  return (
-    <div>PageHeader</div>
-  )
-}
