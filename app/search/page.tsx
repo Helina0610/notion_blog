645380b133c9b35371b255e620a3ac9f0752b2b4
Helina0610 <97938033+Helina0610@users.noreply.github.com>
2024-01-07 17:05:12 +0900

    feat : add Search Function

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,5 @@
 import { SearchHeroSection } from '@/components/search/SearchHeroSection'
 import { SearchInputSction } from '@/components/search/SearchInputSction'
-import { SearchResultSection } from '@/components/search/SearchResultSection'
 import React from 'react'
 
 const Search = () => {
